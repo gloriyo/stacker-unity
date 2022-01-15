fd453b33bcc2089cdb978b82188f3b444cbf6836
gloriyo <gloriajyoon@gmail.com>
2022-01-14 23:39:03 -0800

    basic starter slab movements

---
+++ Assets/Scripts/SlidingSlab.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlidingSlab : MonoBehaviour
+{
+    public static SlidingSlab CurrentSlab { get; private set; }
+    public static SlidingSlab PreviousSlab { get; private set; }
+
+    [SerializeField] 
+    private float moveSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    internal void Stop()
+    {
+        moveSpeed = 0;
+        float overHang = transform.position.x - PreviousSlab.transform.position.x;
+
+        Debug.Log(overHang);
+        
+        float direction = overHang > 0 ? 1f : -1f;
+        SlicedSlab(overHang, direction);
+    }
+
+    void OnEnable()
+    {
+        if (PreviousSlab == null)
+        {
+            PreviousSlab = this;
+        }
+        CurrentSlab = this;
+        GetComponent<Renderer>().material.color = GetRandomColor();
+    }
+
+    private Color GetRandomColor()
+    {
+        Color newColor = new Color(
+            Random.Range(0f, 1f), 
+            Random.Range(0f, 1f), 
+            Random.Range(0f, 1f)
+        );
+        return newColor;
+    }
+
+    void SlicedSlab(float overHang, float direction)
+    {
+        float remainingSize = PreviousSlab.transform.localScale.x - Mathf.Abs(overHang);
+        float hangingSize = PreviousSlab.transform.localScale.x - remainingSize;
+
+        float remainingPosition = PreviousSlab.transform.position.x + (overHang / 2f);
+        transform.localScale = new Vector3(remainingSize, transform.localScale.y, transform.localScale.z);
+        transform.position = new Vector3(remainingPosition, transform.position.y, transform.position.z);
+
+        float hangingPosition = transform.position.x + (remainingSize / 2f * direction) + (hangingSize / 2f * direction);
+
+        SpawnDroppingSlab(hangingPosition, hangingSize);
+    }
+
+    void SpawnDroppingSlab(float fallingSlabPosition, float fallingSlabSize)
+    {
+        var slab = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        slab.transform.localScale = new Vector3(fallingSlabSize, transform.localScale.y, transform.localScale.z);
+        slab.transform.position = new Vector3(fallingSlabPosition, transform.position.y, transform.position.z);
+
+        slab.AddComponent<Rigidbody>();
+        Destroy(slab.gameObject, 1f);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.right * Time.deltaTime * moveSpeed;
+    }
+}
